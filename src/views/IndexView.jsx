5b04eddd8bf4690004f7ac82d69b0b3051fe5b79
kim_haaga <kim.haaga@live.se>
2021-07-19 15:44:50 +0200

    Setup font-family and weight, add material ui grid to index view

--- src/views/IndexView.jsx
+++ src/views/IndexView.jsx
@@ -1,24 +1,28 @@
 import React from "react"
 import LogoCHWL from "../assets/LogoCHWL.png"
+import { Grid } from "@material-ui/core"
 
 const IndexView = () => {
-
   return (
     <>
-      <div style={styles.screenSplitContainer}>
-        <div style={styles.cell}>
+      <Grid container justifyItems="center">
+        <Grid item xs={12} md={6}>
           <img
             src={LogoCHWL}
             data-cy="logo"
-            style={{ width: "100%" }}
+            style={styles.image}
             alt="Community Health West London"
           />
-          <h3 data-cy="mission-statement" style={styles.missionStatement}>
+          <h3 data-cy="mission-statement" style={styles.statement}>
             Our aim is to improve people's health and well-being
           </h3>
-        </div>
-        <div style={styles.cell}></div>
-      </div>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <div>
+            <h1 style={styles.slider}>Richard's Story</h1>
+          </div>
+        </Grid>
+      </Grid>
     </>
   )
 }
@@ -26,20 +30,20 @@ const IndexView = () => {
 export default IndexView
 
 const styles = {
-  screenSplitContainer: {
-    display: "flex",
-    flexDirection: "row",
+  image: {
     width: "100%",
-    height: "80vh",
-    alignItems: "center",
-  },
-  missionStatement: {
-    fontSize: "24px",
-    margin: "0 50px",
+    marginTop: "13%",
+    marginLeft: "auto",
+    paddingRight: "auto",
     textAlign: "center",
   },
-  cell: {
-    width: "50%",
-    padding: "50px",
+  statement: {
+    textAlign: "center",
+    fontSize: "36px"
+  },
+  slider: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: "36px"
   },
 }
