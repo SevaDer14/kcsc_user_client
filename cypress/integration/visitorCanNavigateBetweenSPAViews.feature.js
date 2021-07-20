360054ed4750a4548b39e221b9d0c920ddcf2d9c
luke <lukeperrin27@gmail.com>
2021-07-20 13:04:56 +0200

    sets up first tests for services page

--- cypress/integration/visitorCanNavigateBetweenSPAViews.feature.js
+++ cypress/integration/visitorCanNavigateBetweenSPAViews.feature.js
@@ -35,4 +35,22 @@ describe("visitor can navigate between views", () => {
       cy.get("[data-cy=header-subtitle]").should("contain.text", "Search");
     });
   });
+  describe("Services View", () => {
+    before(() => {
+      cy.visit("/services");
+      cy.intercept('GET', '**/api/services', {fixture: 'services.json'})
+    });
+    it("is expected to display service page ", () => {
+      cy.get("[data-cy=service-section]").should("have.length", 6);
+      cy.get("[data-cy=service-section]")
+        .first()
+        .within(() => {
+          cy.get("[data-cy=header]").should("contain.text", "Find a service");
+          cy.get("[data-cy=description]").should(
+            "contain.text",
+            "On this page, you can find all available services in your area"
+          );
+        });
+    });
+  });
 });
