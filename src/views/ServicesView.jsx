aac9fe7eb08aa67beaeb9eb2ce52a301f087a2e7
kim_haaga <kim.haaga@live.se>
2021-07-20 14:27:23 +0200

    WIP layout for services update fixture with url and alt key Add section component

--- src/views/ServicesView.jsx
+++ src/views/ServicesView.jsx
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+import Section from "../components/Section";
 
 const ServicesView = () => {
-  const [sections, setSections] = useState([])
+  const [sections, setSections] = useState([]);
 
-  useEffect( async () => {
-    let response = await axios.get('/services')
-    setSections(response.data.sections)
+  useEffect(() => {
+    const fetchPageData = async () => {
+      let response = await axios.get("/services");
+      setSections(response.data.sections);
+    };
+    fetchPageData();
   }, []);
 
-  return <></>;
+  const sectionList = sections.map((section) => {
+    const { id, header, description, image, buttons } = section;
+    return (
+      <Grid item>
+        <Section
+          id={id}
+          header={header}
+          description={description}
+          image={image}
+          buttons={buttons}
+        />
+      </Grid>
+    );
+  });
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="stretch"
+    >
+      {sectionList}
+    </Grid>
+  );
 };
 
 export default ServicesView;
