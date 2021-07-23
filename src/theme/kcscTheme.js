fe9274072e8e99b053e3fa1280653784a7f34050
sevader14 <vsevolod.deriushkin@gmail.com>
2021-07-23 11:12:37 +0200

    adds theme and fonts files

---
+++ src/theme/kcscTheme.js
@@ -0,0 +1,62 @@
+import NexaLight from "../assets/fonts/Nexa-Light.woff2";
+import NexaBold from "../assets/fonts/Nexa-Bold.woff2";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const nexaLight = {
+  fontFamily: "Nexa",
+  fontStyle: "normal",
+  fontDisplay: "swap",
+  fontWeight: "400",
+  src: `
+   local('Nexa'),
+   local('Nexa-Light'),
+   url(${NexaLight}) format('woff2')
+ `,
+  unicodeRange:
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+};
+
+const nexaBold = {
+  fontFamily: "Nexa",
+  fontStyle: "bold",
+  fontDisplay: "swap",
+  fontWeight: "800",
+  src: `
+   local('Nexa'),
+   local('Nexa-Bold'),
+   url(${NexaBold}) format('woff2')
+ `,
+  unicodeRange:
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+};
+
+const kcscTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+      contrastText: "#6a0dad ",
+    },
+    secondary: {
+      main: "#E86406",
+      contrastText: "#fff ",
+    },
+  },
+  typography: {
+    fontFamily: "Nexa",
+    body1: {
+      fontSize: 24,
+    },
+    button: {
+      fontSize: 24,
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [nexaLight, nexaBold],
+      },
+    }
+  }
+});
+
+export default kcscTheme
