60fcf9603713c75a9a0aa47cedbb33cff73028a9
sevader14 <vsevolod.deriushkin@gmail.com>
2021-07-23 15:40:32 +0200

    displays map

--- src/views/ServiceSearchView.jsx
+++ src/views/ServiceSearchView.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import ServiceSearch from "../components/ServiceSearch";
 import ServiceListing from "../components/ServiceListing";
+import Map from "../components/Map";
 
 const SearchSelfCareView = () => {
   return (
@@ -11,6 +12,7 @@ const SearchSelfCareView = () => {
       </Helmet>
       <h3 data-cy="header-subtitle">Search for services</h3>
       <ServiceSearch />
+      <Map/>
       <ServiceListing />
     </>
   );
