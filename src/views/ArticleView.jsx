4661022395c8d244239272a9359393e19474db44
luke <lukeperrin27@gmail.com>
2021-07-29 15:06:45 +0200

    removes visitor can navigate using header in mobile folder until chore to fix this is completed adds styling to single article view update single article fixture file with multiple paragraph entry adds testing in mobile enviorments for single article view

--- src/views/ArticleView.jsx
+++ src/views/ArticleView.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Articles from "../modules/Articles";
-import {
-  Container,
-  Typography,
-  Divider,
-  Grid,
-} from "@material-ui/core";
+import { Container, Typography, Divider, Grid } from "@material-ui/core";
 
 const ArticleView = () => {
   const { id } = useParams();
   const [article, setArticle] = useState({});
-  const {title, author, date, image, body} = article
-  
+  const { title, author, date, image, body } = article;
+
   useEffect(() => {
     const fetchPageData = async () => {
       let response = await Articles.show(id);
@@ -22,13 +17,13 @@ const ArticleView = () => {
   }, [id]);
 
   return (
-    <Container data-cy="article" maxWidth="md">
+    <Container style={styles.container} data-cy="article" maxWidth="md">
       {image ? (
         <>
           <Typography component="h3" variant="h3" data-cy="title">
             {title}
           </Typography>
-          <Grid container>
+          <Grid container justify="space-between" style={styles.information}>
             <Grid item>
               <Typography
                 component="p"
@@ -42,18 +37,29 @@ const ArticleView = () => {
               </Typography>
             </Grid>
           </Grid>
-          <Divider orientation="vertical" variant="middle" />
+          <Divider
+            style={styles.divider}
+            orientation="horizontal"
+            variant="fullWidth"
+          />
           <img
             data-cy="image"
             src={image.url}
             alt={image.alt}
             style={styles.image}
           />
-          <Typography component="p" variant="body1" data-cy="body">
+          <Typography
+            component="p"
+            variant="body1"
+            data-cy="body"
+            style={styles.body}
+          >
             {body}
           </Typography>
         </>
-      ) : <p>No article here</p>}
+      ) : (
+        <p>No article here</p>
+      )}
     </Container>
   );
 };
@@ -61,10 +67,23 @@ const ArticleView = () => {
 export default ArticleView;
 
 const styles = {
+  container: {
+    marginTop: "124px",
+  },
+  information: {
+    marginTop: "12px",
+  },
   image: {
     width: "100%",
-    margin: "50px 0",
-    height: 500,
+    margin: "12px 0",
+    maxHeight: "500px",
+    minHeight: '200px',
     objectFit: "cover",
+  },
+  divider: {
+    marginBottom: "12px",
+  },
+  body: {
+    whiteSpace: "pre-wrap",
   },
 };
