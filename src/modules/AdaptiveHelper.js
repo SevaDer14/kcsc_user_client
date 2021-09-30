bece7b0d3b39df0b5444f11bc0f87567689bf523
sevader14 <vsevolod.deriushkin@gmail.com>
2021-09-30 11:58:39 +0200

    adds handling of undefined main_tabs in AdaptiveHelper

--- src/modules/AdaptiveHelper.js
+++ src/modules/AdaptiveHelper.js
@@ -6,20 +6,20 @@ const AdaptiveHelper = {
     setActiveSecondaryTab
   ) {
     const routeToActiveTabMap = {
-      [main_tabs[0].link]: [0, 0],
+      [main_tabs[0]?.link]: [0, 0],
       "#section-1": [1, 0],
       "#section-2": [1, 1],
       "#section-3": [1, 2],
       "#section-4": [1, 3],
       "#section-5": [1, 4],
-      [main_tabs[1].link]: [1, 0],
-      [main_tabs[2].secondary_tabs[0].link]: [2, 0],
-      [main_tabs[2].secondary_tabs[1].link]: [2, 1],
-      [main_tabs[2].link]: [2, 0],
-      [main_tabs[3].secondary_tabs[0].link]: [3, 0],
-      [main_tabs[3].secondary_tabs[1].link]: [3, 1],
-      [main_tabs[3].link]: [3, 0],
-      [main_tabs[4].link]: [4, 0],
+      [main_tabs[1]?.link]: [1, 0],
+      [main_tabs[2]?.secondary_tabs[0].link]: [2, 0],
+      [main_tabs[2]?.secondary_tabs[1].link]: [2, 1],
+      [main_tabs[2]?.link]: [2, 0],
+      [main_tabs[3]?.secondary_tabs[0].link]: [3, 0],
+      [main_tabs[3]?.secondary_tabs[1].link]: [3, 1],
+      [main_tabs[3]?.link]: [3, 0],
+      [main_tabs[4]?.link]: [4, 0],
     };
 
     for (let key of Object.keys(routeToActiveTabMap)) {
