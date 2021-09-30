bf83a5c85ff7fe744ce3e5f82bac28bd9b814936
sevader14 <vsevolod.deriushkin@gmail.com>
2021-09-30 11:54:31 +0200

    constructs routeToActiveTabMap programmatically

--- src/modules/AdaptiveHelper.js
+++ src/modules/AdaptiveHelper.js
@@ -1,27 +1,32 @@
 const AdaptiveHelper = {
-  muiActiveTabSelect(currentUrl, setActiveMainTab, setActiveSecondaryTab) {
+  muiActiveTabSelect(
+    currentUrl,
+    main_tabs,
+    setActiveMainTab,
+    setActiveSecondaryTab
+  ) {
     const routeToActiveTabMap = {
-      "/home": [0, 0],
-      "#find-a-self-care-service": [1, 0],
-      "#long-term-self-care": [1, 1],
-      "#mental-health": [1, 2],
-      "#north-kensington-self-care": [1, 3],
-      "#find-a-link-workers": [1, 4],
-      "/services": [1, 0],
-      "/organization": [2, 0],
-      "/self_care": [2, 1],
-      "/about": [2, 0],
-      "/news_info/news": [3, 0],
-      "/information": [3, 1],
-      "/news_info": [3, 0],
-      "/contact": [4, 0],
+      [main_tabs[0].link]: [0, 0],
+      "#section-1": [1, 0],
+      "#section-2": [1, 1],
+      "#section-3": [1, 2],
+      "#section-4": [1, 3],
+      "#section-5": [1, 4],
+      [main_tabs[1].link]: [1, 0],
+      [main_tabs[2].secondary_tabs[0].link]: [2, 0],
+      [main_tabs[2].secondary_tabs[1].link]: [2, 1],
+      [main_tabs[2].link]: [2, 0],
+      [main_tabs[3].secondary_tabs[0].link]: [3, 0],
+      [main_tabs[3].secondary_tabs[1].link]: [3, 1],
+      [main_tabs[3].link]: [3, 0],
+      [main_tabs[4].link]: [4, 0],
     };
 
     for (let key of Object.keys(routeToActiveTabMap)) {
       if (currentUrl.includes(key)) {
         setActiveMainTab(routeToActiveTabMap[key][0]);
         setActiveSecondaryTab(routeToActiveTabMap[key][1]);
-        break
+        break;
       }
     }
   },
