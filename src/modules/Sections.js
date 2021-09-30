508fbad48cdbddbcf40d1e6330c574b011c1b5bf
Seva Deriushkin <79849155+SevaDer14@users.noreply.github.com>
2021-09-30 11:39:57 +0200

    Removes comment

--- src/modules/Sections.js
+++ src/modules/Sections.js
@@ -5,7 +5,6 @@ const Sections = {
   async index(view) {
     try {
       const response = await axios.get(`/sections?view=${view}`);
-      // const response = await axios.get(`/sections?view=about_us`);
       return response.data.sections;
     } catch (e) {
       store.dispatch({ type: "SET_LAYOUT_LOAD_ERROR", payload: true });
